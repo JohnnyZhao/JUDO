5d1585af7b469e53a34f734ca15b50315f75545d
gato <phoenixlv0108@gmail.com>
2012-03-16 23:52:09 +0800

    tests.py modified for test push

--- judoclub/checkin/tests.py
+++ judoclub/checkin/tests.py
@@ -1,7 +1,7 @@
 """
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
-
+This line is added for testing
 Replace this with more appropriate tests for your application.
 """
 
